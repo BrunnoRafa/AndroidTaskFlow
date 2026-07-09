d1558604fcf0501e61e8e963758fc35e9a3f23a8
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2026-07-09 13:55:39 -0300

    feat: implementando Scaffold para organizar o template

--- app/src/main/java/com/brunorafael/taskflow/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/brunorafael/taskflow/ui/screens/home/HomeScreen.kt
@@ -1,8 +1,12 @@
 package com.brunorafael.taskflow.ui.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.brunorafael.taskflow.ui.components.AddTaskButton
@@ -11,9 +15,22 @@ import com.brunorafael.taskflow.ui.components.HomeTopBar
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        HomeTopBar()
-        EmptyState()
-        AddTaskButton()
+    Scaffold(
+        topBar = {
+            HomeTopBar()
+        },
+        floatingActionButton = {
+            AddTaskButton()
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            EmptyState()
+        }
     }
 }
